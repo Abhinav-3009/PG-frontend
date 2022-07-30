63f7daa50b6ffd9e378c0d95944ed28f9c35c223
kame-style <gouravad12@gmail.com>
2022-07-30 20:00:31 +0530

    Institute Reg Form

---
+++ src/app/iinstitute.ts
@@ -0,0 +1,20 @@
+export interface IInstitute {
+    InstituteCategory:string
+    Name:string
+    Institutecode:number
+    Disecode:number
+    Location:string
+    InstituteType:string
+    AffiliatedState:string
+    AffiliatedName:string
+    AdmissionStartYear:string
+    Password:string
+    Address:string
+    City:string
+    State:string
+    District:string
+    Pincode:number
+    PrincipalName:string
+    PrincipalNumber:string
+}
+
